40b5ec3ae5b25cebc94e57b87bfd375b79daff8f
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-13 21:19:35 +0000

    fix MIME type

--- app/manifest.js
+++ app/manifest.js
@@ -1,23 +1,23 @@
 export default function manifest() {
-    return {
-      name: 'Next.js PWA',
-      short_name: 'NextPWA',
-      description: 'A Progressive Web App built with Next.js',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      icons: [
-        {
-          src: '/PWA_192.svg',
-          sizes: '192x192',
-          type: 'image/svg',
-        },
-        {
-            src: '/PWA_512.svg',
-            sizes: '512x512',
-            type: 'image/svg',
-          },
-      ],
-    }
-  }
+  return {
+    name: "Next.js PWA",
+    short_name: "NextPWA",
+    description: "A Progressive Web App built with Next.js",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/PWA_192.svg",
+        sizes: "192x192",
+        type: "image/svg+xml",
+      },
+      {
+        src: "/PWA_512.svg",
+        sizes: "512x512",
+        type: "image/svg+xml",
+      },
+    ],
+  };
+}
